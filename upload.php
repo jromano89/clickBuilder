5b6f5bd2cb0b013a9186376737d8c12182301562
jromano89 <jerrodromano@gmail.com>
2021-03-29 13:01:09 -0400

    Changes to file upload logic and styling

--- upload.php
+++ upload.php
@@ -1,4 +1,8 @@
 <?php
-$dir = "headers/";
-move_uploaded_file($_FILES["image"]["tmp_name"], $dir. $_FILES["image"]["name"]);
+ini_set("display_errors", 1);
+ini_set('error_reporting', E_ALL);
+$target_dir = "headers/";
+$target_file = $target_dir . basename($_FILES["customHeader"]["name"]);
+move_uploaded_file($_FILES["customHeader"]["tmp_name"], $target_file);
 ?>
+
